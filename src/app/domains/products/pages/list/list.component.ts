208d03819458dd25989bbed018371cee33042085
Mauro Alice <alicemauroe@gmail.com>
2025-02-08 18:50:17 -0300

    getting product data,adding gallery of images and details of gallery

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,14 +2,13 @@ import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component'
 import { Product } from '@shared/models/product.model'
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
